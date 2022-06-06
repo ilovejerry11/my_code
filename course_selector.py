c4ad5daee422e6635737b0fe3859025386f6485c
ilovejerry11 <86518582+ilovejerry11@users.noreply.github.com>
2022-06-07 01:25:25 +0800

    Update course_selector.py

--- course_selector.py
+++ course_selector.py
@@ -1,44 +1,46 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
-# webdriver執行黨的位置
-path = "" 
-driver = webdriver.Chrome(path) # or other webdriver
+parameter = '&' + input('請輸入網址').split('&', maxsplit=1)[1]
+row = input('請輸入課程在第幾列') # 比實際的列數多一
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# webdriver執行檔的位置
+path = "C:/Users/user/Downloads/edgedriver_win64/msedgedriver.exe"
+driver = webdriver.Edge(path)
 
 # 選課登陸頁面
-driver.get("https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/login.php?m=0")
-
-id = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/font/center/form/table/tbody/tr[2]/td[2]/input'))
-)
-password = driver.find_element_by_xpath('/html/body/font/center/form/table/tbody/tr[3]/td[2]/input')
-
-#輸入學號、密碼
-id.send_keys("")
-password.send_keys("")
-id.send_keys(Keys.ENTER)
-
-# need to modify
-driver.get("https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/Add_Course01.cgi?session_id=V0zYxKwgMhgYBD32tlnzWImT9fTbkQgqs926&dept=F000&grade=2&page=0")
+login_url = 'https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/login.php?m=0'
 
 while True:
-    # driver.refresh()
-    remaining = driver.find_element_by_xpath('')
-    print(remaining.text)
-    if (int(remaining.text) > 0):
-        # 依照想上的課程修改
-        check_box = driver.find_element_by_xpath('')
-        check_box.click()
-        submit = driver.find_element_by_xpath('/html/body/center/form/table/tbody/tr[2]/th/input')
-        submit.click()
-        # driver.quit()
-        # break
-    time.sleep(30)
+    driver.get(login_url)
+    id = driver.find_element_by_name('id')
+    password = driver.find_element_by_name('password')
+
+    # 輸入學號、密碼
+    id.send_keys("409410015")
+    password.send_keys("Jerry1122@")
+    password.submit()
+
+    add_course_url = driver.find_element_by_link_text('加選及加簽').get_attribute('href')
+    session_id = add_course_url.split('=')[1]
+
+    front = 'https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/Add_Course01.cgi?session_id='
+    url = (front + session_id + parameter)
+    driver.get(url)
+
+    for i in range(30):
+        driver.refresh()
+        course_name = driver.find_element_by_xpath('/html/body/center/form/table/tbody/tr[1]/th/table/tbody/tr[' + row + ']/th[4]').text
+        remaining = driver.find_element_by_xpath('/html/body/center/form/table/tbody/tr[1]/th/table/tbody/tr[' + row + ']/th[3]').text
+        print(course_name, remaining) # 確認有沒有選對課程
+        if (int(remaining) > 0):
+            check_box = driver.find_element_by_xpath('/html/body/center/form/table/tbody/tr[1]/th/table/tbody/tr['+ row +']/th[1]/input')
+            check_box.click()
+            check_box.submit()
+            driver.quit()
+            break
+        else:
+            time.sleep(10)
     
 
 # time.sleep(10)
